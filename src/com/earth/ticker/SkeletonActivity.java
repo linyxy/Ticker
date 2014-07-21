fc76a8a97f29d2d02d198a1741be15e6add064de
linyxy <linyxy.art@gmail.com>
2014-07-21 23:49:12 +0800

    重新定制了new的页面。增加了space填充小格子。修改了editText的问题。目前行列已经对齐。缺少：一个复选框行。56个icon

--- src/com/earth/ticker/SkeletonActivity.java
+++ src/com/earth/ticker/SkeletonActivity.java
@@ -130,7 +130,7 @@ public class SkeletonActivity extends ActionBarActivity implements
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_skeleton,
+			View rootView = inflater.inflate(R.layout.fragment_new_event,
 					container, false);
 			return rootView;
 		}
