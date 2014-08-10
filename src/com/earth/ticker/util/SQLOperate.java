0b570640e70c3abbb278f143c66029656362b1ef
linyxy <linyxy.art@gmail.com>
2014-08-10 12:48:16 +0800

    继续添加后端数据库类： 加入 删除事件，删除文件夹，删除文件夹和相关事件

--- src/com/earth/ticker/util/SQLOperate.java
+++ src/com/earth/ticker/util/SQLOperate.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
@@ -31,7 +32,7 @@ public class SQLOperate {
 		ContentValues cv = new ContentValues();
 		db.beginTransaction();// 开始事务
 		try {
-			Log.d(DBtag,cv.toString());
+			Log.d(DBtag, cv.toString());
 			db.setTransactionSuccessful();// 调用此方法会在执行到endTransaction()
 											// 时提交当前事务，如果不调用此方法会回滚事务
 			ifSuccessful = true;
@@ -42,10 +43,10 @@ public class SQLOperate {
 		db.close();
 		return ifSuccessful;
 	}
-	
-	
+
 	/**
 	 * method to add a new event
+	 * 
 	 * @param ctx
 	 * @param name
 	 * @param folder
@@ -60,10 +61,9 @@ public class SQLOperate {
 	 * @param event_status
 	 * @return
 	 */
-	public static boolean addEvent(Context ctx,String name,
-			String folder,String icon,
-			String time_start,String duration,String repeat,
-			String alarm,String alarm_name, String alarm_address,
+	public static boolean addEvent(Context ctx, String name, String folder,
+			String icon, String time_start, String duration, String repeat,
+			String alarm, String alarm_name, String alarm_address,
 			String extra, String event_status) {
 		DatabaseHelper databaseHelper = new DatabaseHelper(ctx);
 		SQLiteDatabase db = databaseHelper.getWritableDatabase();
@@ -87,7 +87,7 @@ public class SQLOperate {
 		cv.put("event_status", event_status);
 		db.beginTransaction();// 开始事务
 		try {
-			Log.d(DBtag,cv.toString());
+			Log.d(DBtag, cv.toString());
 			db.insert("events", null, cv);
 			db.setTransactionSuccessful();
 			ifSuccessful = true;
@@ -121,7 +121,7 @@ public class SQLOperate {
 		cv.put("last_change_date", time_stamp);
 		db.beginTransaction();// 开始事务
 		try {
-			Log.d(DBtag,cv.toString());
+			Log.d(DBtag, cv.toString());
 			db.insert("notes", null, cv);
 			db.setTransactionSuccessful();
 			ifSuccessful = true;
@@ -133,10 +133,6 @@ public class SQLOperate {
 		db.close();
 		return ifSuccessful;
 	}
-	
-
-	
-	
 
 	/**
 	 * method insert a sub event and relate to event
@@ -164,7 +160,7 @@ public class SQLOperate {
 		cv.put("state", "1");
 		db.beginTransaction();// 开始事务
 		try {
-			Log.d(DBtag,cv.toString());
+			Log.d(DBtag, cv.toString());
 			long subEventId = db.insert("sub_events", null, cv);
 			relateESubE(ctx, related_event_id, subEventId);
 			db.setTransactionSuccessful();
@@ -177,7 +173,7 @@ public class SQLOperate {
 		db.close();
 		return ifSuccessful;
 	}
-	
+
 	/**
 	 * method to add event into folder
 	 * 
@@ -195,7 +191,7 @@ public class SQLOperate {
 		cv.put("name", name);
 		db.beginTransaction();// 开始事务
 		try {
-			Log.d(DBtag,cv.toString());
+			Log.d(DBtag, cv.toString());
 			db.insert("event_contact_work_related", null, cv);
 			db.setTransactionSuccessful();
 			ifSuccessful = true;
@@ -229,7 +225,7 @@ public class SQLOperate {
 		cv.put("work", work);
 		db.beginTransaction();// 开始事务
 		try {
-			Log.d(DBtag,cv.toString());
+			Log.d(DBtag, cv.toString());
 			db.insert("event_contact_work_related", null, cv);
 			db.setTransactionSuccessful();
 			ifSuccessful = true;
@@ -258,7 +254,7 @@ public class SQLOperate {
 		cv.put("sub_event_id", subEventId);
 		db.beginTransaction();// 开始事务
 		try {
-			Log.d(DBtag,cv.toString());
+			Log.d(DBtag, cv.toString());
 			db.insert("event_sub_event_related", null, cv);
 			db.setTransactionSuccessful();
 			ifSuccessful = true;
@@ -288,7 +284,7 @@ public class SQLOperate {
 		cv.put("note_id", noteId);
 		db.beginTransaction();// 开始事务
 		try {
-			Log.d(DBtag,cv.toString());
+			Log.d(DBtag, cv.toString());
 			db.insert("event_note_related", null, cv);
 			db.setTransactionSuccessful();
 			ifSuccessful = true;
@@ -299,6 +295,109 @@ public class SQLOperate {
 		}
 		db.close();
 		return ifSuccessful;
+	}
+
+	/**
+	 * method to delete an event
+	 * 
+	 * @param ctx
+	 * @param eventId
+	 * @return
+	 */
+	public static boolean deleteEvent(Context ctx, long eventId) {
+		DatabaseHelper databaseHelper = new DatabaseHelper(ctx);
+		SQLiteDatabase db = databaseHelper.getWritableDatabase();
+		boolean ifSuccessful = false;
+		db.beginTransaction();// 开始事务
+		try {
+			String whereClause = "event_id=? ";// 删除的条件
+			String[] whereArgs = { String.valueOf(eventId) };// 删除的条件参数
+			db.delete("events", whereClause, whereArgs);// 执行删除
+			db.setTransactionSuccessful();
+			Log.d(DBtag, "success operation");
+
+			ifSuccessful = true;
+		} finally {
+			db.endTransaction();// 由事务的标志决定是提交事务，还是回滚事务
+		}
+		db.close();
+		return ifSuccessful;
+	}
+
+	/**
+	 * method to delete a folder action would not delete related activity but
+	 * activity are not moved away they may lost some where or bug would occurr
+	 * 
+	 * @param ctx
+	 * @param eventId
+	 * @param name
+	 * @return
+	 */
+	public static boolean deleteFolder(Context ctx, String name) {
+		DatabaseHelper databaseHelper = new DatabaseHelper(ctx);
+		SQLiteDatabase db = databaseHelper.getWritableDatabase();
+		boolean ifSuccessful = false;
+		db.beginTransaction();// 开始事务
+		try {
+			String whereClause = " name=?";// 删除的条件
+			String[] whereArgs = { name };// 删除的条件参数
+			db.delete("event_folder_related", whereClause, whereArgs);// 执行删除
+			db.setTransactionSuccessful();
+			Log.d(DBtag, "success operation");
+			ifSuccessful = true;
+		} finally {
+			db.endTransaction();// 由事务的标志决定是提交事务，还是回滚事务
+		}
+		db.close();
+		return ifSuccessful;
+	}
+
+	/**
+	 * delete the folder with its related events
+	 * 
+	 * @param ctx
+	 * @param name
+	 * @return
+	 */
+	public static boolean deleteFolderREvent(Context ctx, String name) {
+		boolean ifSuccessfulDeleteEvents = false;
+		boolean ifSuccessfulDeleteFolder = false;
+		DatabaseHelper databaseHelper = new DatabaseHelper(ctx);
+		SQLiteDatabase db = databaseHelper.getReadableDatabase();
+		Cursor targetEvents = null;
+		db.beginTransaction();// 开始事务
+		try {
+			String[] col = { "event_id" };
+			String[] args = { name };
+			targetEvents = db.query("event_folder_related", col, "name", args,
+					null, null, null);
+			db.setTransactionSuccessful();
+			Log.d(DBtag, "success operation");
+		} finally {
+			db.endTransaction();// 由事务的标志决定是提交事务，还是回滚事务
+		}
+		db.close();
+
+		// start to delete all the events in the folder
+		if (targetEvents != null) {
+			ifSuccessfulDeleteEvents = true;
+			if (targetEvents.moveToFirst()) {
+				while (!targetEvents.isLast()) {
+					long event_id = targetEvents.getLong(targetEvents
+							.getColumnIndex("event_id"));
+					boolean delete = deleteEvent(ctx, event_id);
+					if (!delete) {
+						ifSuccessfulDeleteEvents = false;
+						Log.d(DBtag, "problems occurs when deleting problem");
+					}
+				}
+			}
+			targetEvents.close();
+		}
+		// delete the folder
+		ifSuccessfulDeleteFolder = deleteFolder(ctx, name);
+
+		return ifSuccessfulDeleteEvents && ifSuccessfulDeleteFolder;
 	}
 
 	/**
@@ -316,7 +415,7 @@ public class SQLOperate {
 		boolean ifSuccessful = false;
 		db.beginTransaction();// 开始事务
 		try {
-			
+
 			String whereClause = "event_id=? AND note_id=?";// 删除的条件
 			String[] whereArgs = { String.valueOf(eventId),
 					String.valueOf(noteId) };// 删除的条件参数
@@ -493,20 +592,21 @@ public class SQLOperate {
 				state);
 		return upName && upIcon && upState;
 	}
-	
+
 	/**
 	 * method to change event to another folder
+	 * 
 	 * @param ctx
 	 * @param event_id
 	 * @param newFolder
 	 * @return
 	 */
-	public static boolean updateFolder(Context ctx, long event_id, String newFolder ) {
+	public static boolean updateFolder(Context ctx, long event_id,
+			String newFolder) {
 		String id = String.valueOf(event_id);
-		boolean upNew = sampleUpdate(ctx, "event_folder_related", "id", id, "name", newFolder);
+		boolean upNew = sampleUpdate(ctx, "event_folder_related", "id", id,
+				"name", newFolder);
 		return upNew;
 	}
-	
-	
 
 }
